b24e9cf74cfe1db2c53c2259e2b84fabdf0ea114
takuyadev <takuya.k.toyokawa@protonmail.com>
2023-04-05 14:53:26 -0700

    Finish parameterized queries

--- teachers.js
+++ teachers.js
@@ -7,18 +7,23 @@ const pool = new Pool({
   database: 'bootcampx',
 });
 
+
+const query = `
+SELECT teachers.name, students.cohort_name FROM assistance_requests 
+LEFT JOIN teachers ON (teachers.id=assistance_requests.teacher_id)
+JOIN (
+  SELECT cohorts.name AS cohort_name, students.id AS id FROM students
+  JOIN cohorts ON (students.cohort_id=cohorts.id)
+  WHERE cohorts.name LIKE $1
+) as students ON (students.id = assistance_requests.student_id)
+GROUP BY teachers.name, students.cohort_name
+ORDER BY teachers.name ASC;
+`
+const cohortName = process.argv[2] || 'MAR12'
+const values = [`%'${cohortName}'%`]
+
 pool
-  .query(`
-    SELECT teachers.name, students.cohort_name FROM assistance_requests 
-    LEFT JOIN teachers ON (teachers.id=assistance_requests.teacher_id)
-    JOIN (
-      SELECT cohorts.name AS cohort_name, students.id AS id FROM students
-      JOIN cohorts ON (students.cohort_id=cohorts.id)
-      WHERE cohorts.name = 'JUL02'
-    ) as students ON (students.id = assistance_requests.student_id)
-    GROUP BY teachers.name, students.cohort_name
-    ORDER BY teachers.name ASC;
-  `)
+  .query(query, values)
   .then((res) => {
     res.rows.forEach((teacher) => {
       console.log(`${teacher.name}: ${teacher.cohort_name}`)
